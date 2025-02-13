a0992341bac1ab53014831e32eb2c9de071a3b0b
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2025-02-13 14:58:24 +0100

    behandling

--- src/app/person/[aktorId]/page.tsx
+++ src/app/person/[aktorId]/page.tsx
@@ -1,5 +1,18 @@
+'use client'
+
 import { ReactElement } from 'react'
+import { Button } from '@navikt/ds-react'
+import { usePathname, useRouter } from 'next/navigation'
+import { v4 } from 'uuid'
 
 export default function Page(): ReactElement {
-    return <b>personview</b>
+    const router = useRouter()
+    const pathname = usePathname()
+    return (
+        <div className="p-16">
+            <Button onClick={() => router.push(pathname + '/' + v4())} variant="secondary-neutral">
+                Start ny behandling
+            </Button>
+        </div>
+    )
 }
--- src/components/Redirect.tsx
+++ src/components/Redirect.tsx
@@ -2,11 +2,14 @@
 
 import React, { ReactElement, useEffect } from 'react'
 import { Heading } from '@navikt/ds-react'
+import { useRouter } from 'next/navigation'
 
 export function Redirect(): ReactElement {
+    const router = useRouter()
+
     useEffect(() => {
-        window.location.href = '/person/1210841234500'
-    }, [])
+        router.push('/person/1210841234500')
+    }, [router])
     return (
         <Heading size="xsmall" level="1">
             videresender
