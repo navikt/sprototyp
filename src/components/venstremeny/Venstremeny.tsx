ef616901ca28ec4c31d725d84b50fa6a833bb1bb
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2025-02-18 11:42:16 +0100

    knapper

--- src/components/venstremeny/Venstremeny.tsx
+++ src/components/venstremeny/Venstremeny.tsx
@@ -1,12 +1,39 @@
 import React, { ReactElement } from 'react'
-import { BodyShort } from '@navikt/ds-react'
+import { Button, DatePicker, Label, useRangeDatepicker, VStack } from '@navikt/ds-react'
 
 import styles from './Venstremeny.module.css'
 
 export const Venstremeny = (): ReactElement => {
     return (
         <section className={styles.Venstremeny}>
-            <BodyShort>Venstremeny</BodyShort>
+            <SykmeldingPeriode />
+
+            <div>
+                <Button variant="tertiary" size="xsmall">
+                    Skriv individuell begrunnelse
+                </Button>
+            </div>
+            <div>
+                <Button size="small">Send til godkjenning</Button>
+            </div>
         </section>
+    )
+}
+
+const SykmeldingPeriode = () => {
+    const { datepickerProps, toInputProps, fromInputProps } = useRangeDatepicker({})
+
+    return (
+        <div>
+            <Label size="small" className="mb-4">
+                Periode
+            </Label>
+            <DatePicker {...datepickerProps}>
+                <VStack gap="4" justify="center" className="pl-4 ">
+                    <DatePicker.Input size="small" {...fromInputProps} label="Fra" />
+                    <DatePicker.Input size="small" {...toInputProps} label="Til" />
+                </VStack>
+            </DatePicker>
+        </div>
     )
 }
