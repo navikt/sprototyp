c4ed94fec2377a5666bda0a92a1d5af9eb5f0553
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2025-02-25 09:17:35 +0100

    tidslinje

--- src/app/person/[aktorId]/layout.tsx
+++ src/app/person/[aktorId]/layout.tsx
@@ -6,6 +6,7 @@ import { PersonHeaderWithContent } from '@/components/personheader/Personheader'
 import { Saksbilde } from '@/components/saksbilde/Saksbilde'
 import { Venstremeny } from '@/components/venstremeny/Venstremeny'
 import { Historikk } from '@/components/historikk/Historikk'
+import { Tidslinje } from '@components/tidslinje/Tidslinje'
 
 import styles from './layout.module.css'
 
@@ -20,12 +21,16 @@ export default function Layout({ children }: PropsWithChildren): ReactElement {
     }
 
     return (
-        <div className={styles.Saksbilde}>
-            <PersonHeaderWithContent person={person} />
-
-            <Venstremeny />
-            <Saksbilde>{children}</Saksbilde>
-            <Historikk />
+        <div>
+            <div>
+                <PersonHeaderWithContent person={person} />
+                <Tidslinje />
+            </div>
+            <div className={styles.Saksbilde}>
+                <Venstremeny />
+                <Saksbilde>{children}</Saksbilde>
+                <Historikk />
+            </div>
         </div>
     )
 }
---
+++ src/components/tidslinje/Tidslinje.tsx
@@ -0,0 +1,34 @@
+import { PencilIcon, PersonIcon } from '@navikt/aksel-icons'
+import { Timeline } from '@navikt/ds-react'
+
+import { useBehandlinger } from '@hooks/queries/useBehandlinger'
+
+export const Tidslinje = () => {
+    const { data: behandlinger } = useBehandlinger()
+    if (!behandlinger) return null
+
+    return (
+        <div className="w-100">
+            <Timeline className="pt-8 pb-16">
+                <Timeline.Row label="Manuell behandling" icon={<PersonIcon aria-hidden />}>
+                    {behandlinger.map((p, i) => (
+                        <Timeline.Period
+                            key={i}
+                            start={new Date(p.fom)}
+                            end={new Date(p.tom)}
+                            status="info"
+                            icon={<PencilIcon aria-hidden />}
+                            statusLabel="Manuell behandliung"
+                        />
+                    ))}
+                </Timeline.Row>
+                <Timeline.Zoom>
+                    <Timeline.Zoom.Button label="3 mnd" interval="month" count={3} />
+                    <Timeline.Zoom.Button label="7 mnd" interval="month" count={7} />
+                    <Timeline.Zoom.Button label="9 mnd" interval="month" count={9} />
+                    <Timeline.Zoom.Button label="1.5 år" interval="year" count={1.5} />
+                </Timeline.Zoom>
+            </Timeline>
+        </div>
+    )
+}
