edc2ce510c64b8ad422c6a9d309a4be63e923898
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2025-03-04 10:03:20 +0100

    søknadtidsløinje

--- src/components/tidslinje/Tidslinje.tsx
+++ src/components/tidslinje/Tidslinje.tsx
@@ -2,11 +2,25 @@ import { PencilIcon, PersonIcon } from '@navikt/aksel-icons'
 import { Timeline } from '@navikt/ds-react'
 
 import { useBehandlinger } from '@hooks/queries/useBehandlinger'
+import { useSoknader } from '@hooks/queries/useSoknader'
+import { RSSoknad } from '@typer/soknad'
 
 export const Tidslinje = () => {
     const { data: behandlinger } = useBehandlinger()
+    const { data: soknader } = useSoknader()
     if (!behandlinger) return null
     if (behandlinger.length == 0) return null
+
+    // grupper søknader på orgnummer sekundært arbeidssituasjon
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const soknaderGruppert = soknader?.reduce((acc: Record<string, RSSoknad[]>, soknad) => {
+        const key = soknad.arbeidsgiver?.orgnummer || '' + soknad.arbeidssituasjon
+        if (!acc[key]) {
+            acc[key] = []
+        }
+        acc[key].push(soknad)
+        return acc
+    }, {})
 
     return (
         <div className="w-100">
@@ -23,11 +37,23 @@ export const Tidslinje = () => {
                         />
                     ))}
                 </Timeline.Row>
+                {Object.entries(soknaderGruppert || {}).map(([label, soknader]) => (
+                    <Timeline.Row key={label} label={label}>
+                        {soknader.map((soknad, i) => (
+                            <Timeline.Period
+                                key={i}
+                                start={new Date(soknad.fom!)}
+                                end={new Date(soknad.tom!)}
+                                status="success"
+                                statusLabel="Sendt"
+                            />
+                        ))}
+                    </Timeline.Row>
+                ))}
                 <Timeline.Zoom>
                     <Timeline.Zoom.Button label="3 mnd" interval="month" count={3} />
-                    <Timeline.Zoom.Button label="7 mnd" interval="month" count={7} />
+                    <Timeline.Zoom.Button label="6 mnd" interval="month" count={7} />
                     <Timeline.Zoom.Button label="9 mnd" interval="month" count={9} />
-                    <Timeline.Zoom.Button label="1.5 år" interval="year" count={1.5} />
                 </Timeline.Zoom>
             </Timeline>
         </div>
